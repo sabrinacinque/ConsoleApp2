a3dac29cbce371ef661aaddaf37c2f0ff01cf597
sabrinacinque <cinque.sabrina@gmail.com>
2024-06-18 13:22:48 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp2/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Persona
+    {
+        string nome;
+        string cognome;
+        int eta;
+
+        public Persona(string nome, string cognome,int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+        public string getCognome()
+        {
+            return cognome;
+        }
+
+
+        public int getEta()
+        {
+            return eta;
+        }
+
+        public string getDettagli()
+        {
+            return getNome() + " , " + getCognome() + " , " + getEta() + " anni";
+        }
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Mario", "Rossi", 45);
+            Persona persona2 = new Persona("Giuseppe", "Verdi", 30);
+
+            Console.WriteLine("dettagli persona1 = " + persona1.getDettagli());
+            Console.WriteLine("dettagli persona2 = " + persona2.getDettagli());
+        }
+    }
+}
